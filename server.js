1b0dd4b3878ac1a4403906a227b3e9af97e17d02
Nam6 <geniusnam24may@gmail.com>
2017-02-20 00:48:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-}); 
-app.get('/article one', function(req,res)
+});
+
+app.get('/article1', function(req,res){
 res.send('article one requested');
 });
 
